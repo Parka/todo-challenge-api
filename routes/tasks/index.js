001eaa0b68d3caca74c1ba7b41763d590311477a
Francisco Casanova Ferro <franciscocasanovaferro@gmail.com>
2020-06-06 20:20:41 -0300

    Add missing tasks endpoints

--- routes/tasks/index.js
+++ routes/tasks/index.js
@@ -5,6 +5,29 @@ const db = require('../../models')
 router.get('/', async function(req, res) {
   const response = await db.Task.findAll()
   res.json(response)
-});
+})
+
+router.post('/', async function(req, res) {
+  const {name} = req.body
+  const response = await db.Task.create({
+    name
+  })
+  res.json(response)
+})
+
+router.patch('/:id', async function(req, res) {
+  const {done} = req.body
+  const {id} = req.params
+  const currentTask = await db.Task.findByPk(id)
+  const updatedTask = await currentTask.set('done', done).save()
+  res.json(updatedTask)
+})
+
+router.delete('/:id', async function(req, res) {
+  const {id} = req.params
+  const task = await db.Task.findByPk(id)
+  await task.set('deleted', true).save()
+  res.status(200).send()
+})
 
 module.exports = router
