fce375e4dda58680b6e7ee2606f0e444c6e663af
Francisco Casanova Ferro <franciscocasanovaferro@gmail.com>
2020-06-06 20:18:55 -0300

    Apply router and basic middleware

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const config = require('./config')
+const cors = require('cors')
+
+const tasksRouter = require('./routes/tasks')
+
+app.use(express.json())
+app.use(cors())
 
 app.listen(config.PORT, function () {
     console.log(`Example app listening on port ${config.PORT}!`)
 })
+
+app.use('/tasks', tasksRouter)
